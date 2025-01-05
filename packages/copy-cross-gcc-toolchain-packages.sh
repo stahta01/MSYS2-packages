276791fdde5e6ccd51903d95485e45784e5d5a1e
Tim Stahlhut <stahta01@gmail.com>
2025-01-04 20:43:39 -0500

    packages: Add copy-cross-gcc-toolchain-packages.sh

---
+++ packages/copy-cross-gcc-toolchain-packages.sh
@@ -0,0 +1,11 @@
+mkdir -p ./cross_gcc_toolchain_MSYS_32bit_packages
+
+rm -f ./cross_gcc_toolchain_MSYS_32bit_packages/*-i686.pkg.tar.zst
+
+cp --preserve=timestamps -f ../mingw-w64-cross-binutils/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ../mingw-w64-cross-headers/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ../mingw-w64-cross-crt/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ../mingw-w64-cross-winpthreads/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ../mingw-w64-cross-windows-default-manifest/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ../mingw-w64-cross-gcc/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ../mingw-w64-cross-zlib/mingw-w64-cross-*-i686.pkg.tar.zst ./cross_gcc_toolchain_MSYS_32bit_packages/
