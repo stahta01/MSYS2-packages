aa4ee1b147088837af0f358a055411b97b77659c
Tim Stahlhut <stahta01@gmail.com>
2025-01-09 18:03:38 -0500

    packages: Edit cleanup-selected-old-dlls.sh

--- packages/cleanup-selected-old-dlls.sh
+++ packages/cleanup-selected-old-dlls.sh
@@ -7,4 +7,8 @@ if [[ ${MSYSTEM_CARCH} == "i686" ]]; then
   if ! pacman -Qo /usr/bin/msys-ssl-1.1.dll ; then
     rm -f /usr/bin/msys-ssl-1.1.dll
   fi
+
+  if ! pacman -Qo /usr/bin/cygisl-23.dll ; then
+    rm -f /usr/bin/cygisl-23.dll
+  fi
 fi
