d9013e5c9f5e2eeffcb15b164711dee858a16f86
Tim Stahlhut <stahta01@gmail.com>
2025-01-09 18:04:14 -0500

    packages: Add load-selected-old-dlls.sh

--- packages/load-selected-old-dlls.sh
+++ packages/load-selected-old-dlls.sh
@@ -5,4 +5,5 @@ if [[ ${MSYSTEM_CARCH} == "i686" ]]; then
   cp --preserve=timestamps --no-clobber ./selected_MSYS_32bit_dlls/msys-isl-22.dll /usr/bin/
   cp --preserve=timestamps --no-clobber ./selected_MSYS_32bit_dlls/msys-ssl-1.1.dll /usr/bin/
 
+  cp --preserve=timestamps --no-clobber ./selected_MSYS_32bit_dlls/cygisl-22.dll /usr/bin/
 fi
