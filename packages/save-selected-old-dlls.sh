b8ce73374acdc3a33469cdc62812f4c5771b3ca8
Tim Stahlhut <stahta01@gmail.com>
2025-01-10 14:16:04 -0500

    packages: Edit save-selected-old-dlls.sh

--- packages/save-selected-old-dlls.sh
+++ packages/save-selected-old-dlls.sh
@@ -4,7 +4,5 @@ if [[ ${MSYSTEM_CARCH} == "i686" ]]; then
 
   cp --no-clobber --preserve=timestamps /usr/bin/msys-isl-22.dll  ./selected_MSYS_32bit_dlls/
   cp --no-clobber --preserve=timestamps /usr/bin/msys-ssl-1.1.dll ./selected_MSYS_32bit_dlls/
-  # Some reason it wants the new DLL with cyg prefix; likely libtool related
-  cp --no-clobber --preserve=timestamps /usr/bin/cygisl-23.dll    ./selected_MSYS_32bit_dlls/cygisl-23.dll
-  cp --no-clobber --preserve=timestamps /usr/bin/msys-isl-23.dll  ./selected_MSYS_32bit_dlls/cygisl-23.dll
+
 fi
