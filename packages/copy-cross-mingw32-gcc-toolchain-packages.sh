33a8d03d88c2c3e849ec831de56c34d88cfe2741
Tim Stahlhut <stahta01@gmail.com>
2025-01-10 11:48:23 -0500

    packages: Edit copy-cross-mingw32-gcc-toolchain-packages.sh

--- packages/copy-cross-mingw32-gcc-toolchain-packages.sh
+++ packages/copy-cross-mingw32-gcc-toolchain-packages.sh
@@ -2,6 +2,7 @@ mkdir -p ./cross_mingw32_gcc_toolchain_MSYS_32bit_packages
 
 rm -f ./cross_mingw32_gcc_toolchain_MSYS_32bit_packages/*-i686.pkg.tar.zst
 
+cp --preserve=timestamps -f ../mingw-w64-cross-binutils/mingw-w64-cross-common-*-i686.pkg.tar.zst ./cross_mingw32_gcc_toolchain_MSYS_32bit_packages/
 cp --preserve=timestamps -f ../mingw-w64-cross-binutils/mingw-w64-cross-mingw32-*-i686.pkg.tar.zst ./cross_mingw32_gcc_toolchain_MSYS_32bit_packages/
 cp --preserve=timestamps -f ../mingw-w64-cross-headers/mingw-w64-cross-mingw32-*-i686.pkg.tar.zst ./cross_mingw32_gcc_toolchain_MSYS_32bit_packages/
 cp --preserve=timestamps -f ../mingw-w64-cross-crt/mingw-w64-cross-mingw32-*-i686.pkg.tar.zst ./cross_mingw32_gcc_toolchain_MSYS_32bit_packages/
